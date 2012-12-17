3989ee3ee7f6ec84de7663938d264e3120a6aa8a
Matthew Schinckel <matt@schinckel.net>
2012-12-17 15:09:36 +1030

    Attempt to take width of scrollbar into account.

--- bootstrap/static/bootstrap/js/jquery.tablescroller.js
+++ bootstrap/static/bootstrap/js/jquery.tablescroller.js
@@ -1,4 +1,18 @@
 (function($, undefined) {
+  // Get the scrollbar width...
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+  $(div).css({
+    width: '100px',
+    height: '100px',
+    overflow: 'scroll',
+    position: 'absolute',
+    top: '-999px'
+  });
+  var Y_SCROLLBAR_WIDTH = div.offsetWidth - div.clientWidth;
+  var X_SCROLLBAR_WIDTH = div.offsetHeight - div.clientHeight;
+  document.body.removeChild(div);
+  
   var resizeHandler = function resizeHandler(table, height) {
     return function(evt){
       var widths;
@@ -6,6 +20,8 @@
       var $thead = $table.find('thead');
       var $tbody = $table.find('tbody');
       var $tfoot = $table.find('tfoot');
+      
+      var body_will_scroll;
       
       var $cells = $table.find('td, th');
       
@@ -20,18 +36,29 @@
       $tbody.css({display: 'table-row-group'});
       $tfoot.css({display: 'table-footer-group'});
       
+      body_will_scroll = $tbody.height() > parseInt(height, 10);
+      
       // Now we can get the widths.
       widths = $.map($cells, function(x){ return $(x).width();});
     
       // Now change the display type for all three table parts to block.
       $thead.css({display: 'block', 'border-bottom-width': '1px', 'border-bottom-style': 'solid'});
       // TODO: Figure out how to not have the double border.
-      $tbody.css({display: 'block', overflow: 'auto', 'max-height': height});
+      $tbody.css({display: 'block', 'overflow-x':'hidden', 'overflow-y': 'auto', 'max-height': height});
       $tfoot.css({display: 'block'});
       
       $.each($cells, function(i, el){ 
         $(el).width(widths[i]);
       });
+      
+      // If $tbody will show scrollbars, and the scrollbars have width,
+      // then we need to reduce the width of the last column by that value.
+      if (Y_SCROLLBAR_WIDTH && body_will_scroll) {
+      $.each($tbody.find('tr'), function(i, row) {
+          var $cell = $(row).find('th,td').last()
+          $cell.width($cell.width() - Y_SCROLLBAR_WIDTH);
+        });
+      }
     };
   };
 
